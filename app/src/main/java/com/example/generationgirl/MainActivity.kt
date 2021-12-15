2176588421c9e49f4a72a2839eddfe7106199b86
natashaval <natasha.valentina1998@gmail.com>
2021-12-15 21:47:58 +0700

    findViewById

--- app/src/main/java/com/example/generationgirl/MainActivity.kt
+++ app/src/main/java/com/example/generationgirl/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.generationgirl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val button = findViewById<Button>(R.id.bt_hello)
+    button.setOnClickListener {
+      Toast.makeText(this, "This is my Toast message!", Toast.LENGTH_LONG).show()
+    }
   }
 }

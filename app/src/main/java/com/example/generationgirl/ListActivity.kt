0466c267780782d3a75a7522f2aaee9de0b840ad
Natasha Santoso <natasha.valentina1998@gmail.com>
2021-12-28 23:32:52 +0700

    remove list

--- app/src/main/java/com/example/generationgirl/ListActivity.kt
+++ app/src/main/java/com/example/generationgirl/ListActivity.kt
@@ -7,21 +7,8 @@ import android.widget.ListView
 import android.widget.Toast
 
 class ListActivity : AppCompatActivity() {
-  private val months = listOf(
-    "January", "February", "March", "April", "May", "June", "July", "August", "September",
-    "October", "November", "December"
-  )
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_list)
-
-    val listView = findViewById<ListView>(R.id.lv_months)
-    val monthAdapter = ArrayAdapter<String>(this, R.layout.item_month, months)
-    listView.adapter = monthAdapter
-
-    listView.setOnItemClickListener { parent, view, position, id ->
-      Toast.makeText(this, "This row $id is ${months[position]}", Toast.LENGTH_SHORT).show()
-    }
   }
 }
--- app/src/main/java/com/example/generationgirl/TwoActivity.kt
+++ app/src/main/java/com/example/generationgirl/TwoActivity.kt
@@ -9,23 +9,8 @@ import android.widget.TextView
 import android.widget.Toast
 
 class TwoActivity : AppCompatActivity() {
-  private val TAG = "TwoActivity"
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_two)
-
-    val buttonClick = findViewById<Button>(R.id.buttonClick)
-
-    buttonClick.setOnClickListener {
-      Toast.makeText(this, "Hi Rookies!", Toast.LENGTH_SHORT).show()
-    }
-
-    val buttonChangeName = findViewById<Button>(R.id.buttonChangeName)
-    val editName = findViewById<EditText>(R.id.editTextName)
-    val textName = findViewById<TextView>(R.id.textName)
-    buttonChangeName.setOnClickListener {
-      textName.text = editName.text
-    }
   }
 }

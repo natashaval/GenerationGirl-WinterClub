c49b9188b863db2dad6e66eea88247bde66f9766
Natasha Santoso <natasha.valentina1998@gmail.com>
2021-12-29 23:42:39 +0700

    intent put extra

--- app/src/main/java/com/example/generationgirl/FirstActivity.kt
+++ app/src/main/java/com/example/generationgirl/FirstActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class FirstActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +12,17 @@ class FirstActivity : AppCompatActivity() {
     setContentView(R.layout.activity_first)
 
     val buttonFirst = findViewById<Button>(R.id.buttonFirst)
+    val editTextFirstName = findViewById<TextView>(R.id.editTextFirstName)
+    val buttonProfile = findViewById<Button>(R.id.buttonProfile)
+
     buttonFirst.setOnClickListener {
       val intent = Intent(this, SecondActivity::class.java)
+      intent.putExtra("firstName", editTextFirstName.text.toString())
+      startActivity(intent)
+    }
+
+    buttonProfile.setOnClickListener {
+      val intent = Intent(this, ProfileActivity::class.java)
       startActivity(intent)
     }
   }
--- app/src/main/java/com/example/generationgirl/SecondActivity.kt
+++ app/src/main/java/com/example/generationgirl/SecondActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,9 @@ class SecondActivity : AppCompatActivity() {
 
     val buttonSecond = findViewById<Button>(R.id.buttonSecond)
     val buttonClose = findViewById<Button>(R.id.buttonClose)
+
+    val textViewFirstName = findViewById<TextView>(R.id.textViewFirstName)
+    textViewFirstName.text = intent.getStringExtra("firstName")
 
     buttonSecond.setOnClickListener {
       val intent = Intent(this, FirstActivity::class.java)
